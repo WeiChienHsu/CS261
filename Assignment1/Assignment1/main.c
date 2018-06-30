a59fd613cc944c8b6549b1e6c737915c9b1f8654
WeiChien Hsu <weichien711@gmail.com>
2018-06-29 21:35:15 -0500

    :construction:

--- Assignment1/Assignment1/main.c
+++ Assignment1/Assignment1/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h> 
 
 struct student {
     char* name;
@@ -16,6 +17,11 @@ struct student {
 
 void make_32(int* num) {
     *num = 32;
+}
+
+void init_student(struct student* s, char* name, float gpa) {
+    s->name = name;
+    s->gpa = gpa;
 }
 
 int main(){
@@ -45,15 +51,33 @@ int main(){
 //    n_ptr = &i;
 //    printf("%d \n", *n_ptr);
     
-    int i, n = 4096;
-    int *nums = (int*)malloc(n * sizeof(int));
-    int count = 0;
-    for(i = 1; i < n; i *= 2) {
-        nums[count] = i;
-        printf("%d \n", nums[count++]);
+//    int i, n = 4096;
+//    int *nums = (int*)malloc(n * sizeof(int));
+//    int count = 0;
+//    for(i = 1; i < n; i *= 2) {
+//        nums[count] = i;
+//        printf("%d \n", nums[count++]);
+//    }
+
+//    struct student *s = malloc(sizeof(struct student));
+//    (*s).name = "Luck";
+//    (*s).gpa = 3.0;
+//    s->name = "Nick";
+//    s->gpa = 5.0;
+//
+    
+    int n = 5, i;
+    struct student* students = malloc(n * sizeof(struct student));
+    
+    for (i = 0; i < n; i++) {
+        init_student(students + i, "Dick", 2 + i);
     }
     
+    for(i = 0; i < n; i++) {
+        printf("%s : %f \n",students[i].name, students[i].gpa);
+    }
     
+    free(students);
     
     return 0;
 }

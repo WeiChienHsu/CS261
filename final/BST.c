3b2e6ffd39b9a08c3ae738f8f543955ecd8a4bb1
WeiChien Hsu <weichien711@gmail.com>
2018-08-13 18:26:22 -0500

    :construction: BST

--- final/BST.c
+++ final/BST.c
@@ -1,14 +1,3 @@
-# Review Final 
-
-## Binary Search Tree
-
-|       | DynamicArr Bag | LL Bag | Ordered Bag | BST Bag |
-|:-----:|:--------------:|:------:|:-----------:|:-------:|
-|Add    | O(1) *         | O(1)   | O(n)        | O(logN) |
-|Contain| O(n)           | O(n)   | O(logN)     | O(logN) |
-|Remove | O(n)           | O(1)   | O(n)        | O(logN) |
-
-```c
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -131,9 +120,3 @@ struct Node _removeListMostChild(struct Node *current) {
 int sizeBSTree(struct BSTree * tree){
   return tree -> cnt;
 }
-```
-
-
-***
-
-
